5ad109b7de42558efa404551f2ec76bb6de5ca93
Mall01 <>
2021-06-07 17:33:24 +0000

    Initial commit

---
+++ TallyFunctionality.py
@@ -0,0 +1,56 @@
+help = "Welcome to tallybot here is a list of the following commands \n \
+$create name [starting tally] \n \
+$remove name \n \
+$tally name [increment_amount] \n \
+$show tally \n \
+"
+class tally:
+  
+  def __init__(self):
+    self.tallyMap = {}
+
+  def createTally(self,message):
+    temp = message.split(" ")
+    if (len(temp) < 2): return "I need a name for the Tally!"
+    name = temp[1]
+    start = 0
+    if(len(temp) > 2):
+      try:
+        start = int(temp[2])
+      except: return temp
+    
+    if name in self.tallyMap: return "Tally " + name + " already exists"
+    self.tallyMap[name] = start
+    return "Tally " + name + " created with count " + str(start)
+
+  def removeTally(self,message):
+    temp = message.split(" ")
+    if (len(temp) < 2): return "I need a name for the Tally!"
+    name = temp[1]
+    if name not in self.tallyMap: return "Tally " + name + " doesn't exist"
+    del self.tallyMap[name]
+    return "Tally " + name + " removed"
+    
+  def printHelp():
+    return help
+
+  def showTally(self,message):
+    temp = message.split(" ")
+    if (len(temp) < 2): return "I need a name for the Tally!"
+    name = temp[1]
+    if name not in self.tallyMap: return "Tally " + name + " doesn't exists"
+    return name + ": " + str(self.tallyMap[name])
+  
+  def addTally(self, message):
+    temp = message.split(" ")
+    if (len(temp) < 2): return "I need a name for the Tally!"
+    name = temp[1]
+    val = 1
+    if(len(temp) > 2):
+      try:
+        val = int(temp[2])
+      except: val = 1
+    if name not in self.tallyMap: return "Tally " + name + " doesn't exist"
+    self.tallyMap[name] += val
+    return "Tally " + name + " now has value " + str(self.tallyMap[name])
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+import os
+import TallyFunctionality
+from KeepAlive import keep_alive
+
+client = discord.Client()
+tracker = TallyFunctionality.tally()
+
+
+@client.event
+async def on_ready():
+    print("Hi there! Im tallyBot, you can learn more about me by using $thelp")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    #await message.channel.send()
+    ''''
+  if message.content.startswith('$t'):
+    await message.channel.send("Hello world!")
+  '''
+    if (message.content.startswith("$help")):
+        await message.channel.send(TallyFunctionality.help)
+
+    if (message.content.startswith("$create")):
+        await message.channel.send(tracker.createTally(message.content))
+
+    if (message.content.startswith("$remove")):
+        await message.channel.send(tracker.removeTally(message.content))
+
+    if (message.content.startswith("$show")):
+        await message.channel.send(tracker.showTally(message.content))
+
+    if (message.content.startswith("$tally")):
+        await message.channel.send(tracker.addTally(message.content))
+
+
+keep_alive()
+client.run(os.getenv('Token'))
